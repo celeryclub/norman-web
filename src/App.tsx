72c09ee7056e6369e9bd57ca4e40eb13dd63960f
Steve Davis <steve@celery.club>
2021-11-27 16:41:08 -0800

    Install react-spectrum and wrap app in Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Provider, defaultTheme } from '@adobe/react-spectrum';
 import CoffeeProvider from './providers/CoffeeProvider';
 import RoastProvider from './providers/RoastProvider';
 import Dashboard from './routes/Dashboard';
@@ -15,38 +16,43 @@ const roastProvider = new RoastProvider();
 
 function App() {
   return (
-    <Router>
-      <h1>Norman ☕️</h1>
-      <div>
-        <nav>
-          <Link to="/">Dashboard</Link>
-          {' | '}
-          <Link to="/coffees">Coffees</Link>
-          {' | '}
-          <Link to="/roasts">Roasts</Link>
-        </nav>
-      </div>
-      <hr />
-      <Routes>
-        <Route path="/" element={<Dashboard roastProvider={roastProvider} />} />
-        <Route
-          path="/coffees"
-          element={<CoffeeList coffeeProvider={coffeeProvider} />}
-        />
-        <Route
-          path="/coffees/:id"
-          element={<CoffeeDetails coffeeProvider={coffeeProvider} />}
-        />
-        <Route
-          path="/roasts"
-          element={<RoastList roastProvider={roastProvider} />}
-        />
-        <Route
-          path="/roasts/:id"
-          element={<RoastDetails roastProvider={roastProvider} />}
-        />
-      </Routes>
-    </Router>
+    <Provider theme={defaultTheme}>
+      <Router>
+        <h1>Norman ☕️</h1>
+        <div>
+          <nav>
+            <Link to="/">Dashboard</Link>
+            {' | '}
+            <Link to="/coffees">Coffees</Link>
+            {' | '}
+            <Link to="/roasts">Roasts</Link>
+          </nav>
+        </div>
+        <hr />
+        <Routes>
+          <Route
+            path="/"
+            element={<Dashboard roastProvider={roastProvider} />}
+          />
+          <Route
+            path="/coffees"
+            element={<CoffeeList coffeeProvider={coffeeProvider} />}
+          />
+          <Route
+            path="/coffees/:id"
+            element={<CoffeeDetails coffeeProvider={coffeeProvider} />}
+          />
+          <Route
+            path="/roasts"
+            element={<RoastList roastProvider={roastProvider} />}
+          />
+          <Route
+            path="/roasts/:id"
+            element={<RoastDetails roastProvider={roastProvider} />}
+          />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
